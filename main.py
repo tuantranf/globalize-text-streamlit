a05c922cddb0bae94070798119be110ce09ae413
Tuan (Mike) Tran <tuantranf@gmail.com>
2023-05-18 18:35:08 +0700

    Update main.py

--- main.py
+++ main.py
@@ -43,18 +43,18 @@ def load_LLM(openai_api_key):
     return llm
 
 st.set_page_config(page_title="Globalize Email", page_icon=":robot:")
-st.header("Globalize Text")
+st.header("Globalize Email")
 
-col1, col2 = st.columns(2)
+# col1, col2 = st.columns(2)
 
-with col1:
-    st.markdown("Often professionals would like to improve their emails, but don't have the skills to do so. \n\n This tool \
-                will help you improve your email skills by converting your emails into a more professional format. This tool \
-                is powered by [LangChain](https://langchain.com/) and [OpenAI](https://openai.com) and made by \
-                [@GregKamradt](https://twitter.com/GregKamradt). \n\n View Source Code on [Github](https://github.com/gkamradt/globalize-text-streamlit/blob/main/main.py)")
+# with col1:
+#     st.markdown("Often professionals would like to improve their emails, but don't have the skills to do so. \n\n This tool \
+#                 will help you improve your email skills by converting your emails into a more professional format. This tool \
+#                 is powered by [LangChain](https://langchain.com/) and [OpenAI](https://openai.com) and made by \
+#                 [@GregKamradt](https://twitter.com/GregKamradt). \n\n View Source Code on [Github](https://github.com/gkamradt/globalize-text-streamlit/blob/main/main.py)")
 
-with col2:
-    st.image(image='TweetScreenshot.png', width=500, caption='https://twitter.com/DannyRichman/status/1598254671591723008')
+# with col2:
+#     st.image(image='TweetScreenshot.png', width=500, caption='https://twitter.com/DannyRichman/status/1598254671591723008')
 
 st.markdown("## Enter Your Email To Convert")
 
@@ -89,19 +89,25 @@ def update_text_with_example():
     print ("in updated")
     st.session_state.email_input = "Sally I am starts work at yours monday from dave"
 
+def submit():
+    print("submit")
+    if email_input:
+        st.warning('Please insert email', icon="⚠️")
+            st.stop()
+            
+        if not openai_api_key:
+            st.warning('Please insert OpenAI API Key. Instructions [here](https://help.openai.com/en/articles/4936850-where-do-i-find-my-secret-api-key)', icon="⚠️")
+            st.stop()
+
+        llm = load_LLM(openai_api_key=openai_api_key)
+        prompt_with_email = prompt.format(tone=option_tone, dialect=option_dialect, email=email_input)
+        formatted_email = llm(prompt_with_email)
+        st.write(formatted_email)
+    
+    
+st.button("Globalize it", type='primary', help="Click to convert it", on_click=submit)
+
 st.button("*See An Example*", type='secondary', help="Click to see an example of the email you will be converting.", on_click=update_text_with_example)
 
 st.markdown("### Your Converted Email:")
 
-if email_input:
-    if not openai_api_key:
-        st.warning('Please insert OpenAI API Key. Instructions [here](https://help.openai.com/en/articles/4936850-where-do-i-find-my-secret-api-key)', icon="⚠️")
-        st.stop()
-
-    llm = load_LLM(openai_api_key=openai_api_key)
-
-    prompt_with_email = prompt.format(tone=option_tone, dialect=option_dialect, email=email_input)
-
-    formatted_email = llm(prompt_with_email)
-
-    st.write(formatted_email)
